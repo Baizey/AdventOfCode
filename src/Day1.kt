5567b92cb225ba49702590c1978c21d7b9119b4d
Baizey <lassemoerk.mrk@gmail.com>
2023-12-01 20:18:15 +0100

    first

---
+++ src/Day1.kt
@@ -0,0 +1,65 @@
+fun main() {
+    val content = Input.day(1)
+
+    fun part1(): Long {
+        var sum = 0L
+        content.split("\n")
+            .forEach { line ->
+                val first = line.firstOrNull { it.isDigit() } ?: return@forEach
+                val last = line.last { it.isDigit() }
+                sum += "$first$last".toLong()
+            }
+        return sum
+    }
+
+    fun part2(): Long {
+        var sum = 0L
+        val digits = listOf("__zero__", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        content.split("\n").forEach { line ->
+            var first: Char? = null
+            var firstIndex = Int.MAX_VALUE
+            digits.forEachIndexed { i, v ->
+                if (line.contains(v)) {
+                    val index = line.indexOf(v)
+                    if (index < firstIndex) {
+                        firstIndex = index
+                        first = ("$i")[0]
+                    }
+                }
+            }
+            val firstDigit = line.firstOrNull { it.isDigit() }
+            if (firstDigit != null) {
+                val index = line.indexOf(firstDigit)
+                if (index < firstIndex) {
+                    firstIndex = index
+                    first = firstDigit
+                }
+            }
+            var last: Char? = null
+            var lastIndex = Int.MIN_VALUE
+            digits.forEachIndexed { i, v ->
+                if (line.contains(v)) {
+                    val index = line.lastIndexOf(v)
+                    if (index > lastIndex) {
+                        lastIndex = index
+                        last = ("$i")[0]
+                    }
+                }
+            }
+            val lastDigit = line.lastOrNull { it.isDigit() }
+            if (lastDigit != null) {
+                val index = line.lastIndexOf(lastDigit)
+                if (index > lastIndex) {
+                    lastIndex = index
+                    last = lastDigit
+                }
+            }
+            if (first == null || last == null) return@forEach
+            sum += "$first$last".toLong()
+        }
+        return sum
+    }
+
+    println("Part 1: ${part1()}")
+    println("Part 2: ${part2()}")
+}
---
+++ src/Input.kt
@@ -0,0 +1,34 @@
+import java.io.File
+import java.net.URL
+
+object Input {
+    private val cache: Array<String> = Array(30) { "" }
+
+    fun day(day: Int): String {
+        if (cache[day].isNotEmpty()) return cache[day]
+
+        val fileName = "day_$day.txt"
+        val resource: URL? = object {}.javaClass.classLoader.getResource(fileName)
+        if (resource != null) {
+            val content = resource.openStream().bufferedReader().readLines().joinToString(separator = "\n")
+            cache[day] = content
+            if (content.isNotEmpty()) return content
+        }
+
+        val fileCache = File("C:\\Repositories\\AdventOfCode2023\\resources\\$fileName")
+        fileCache.createNewFile()
+
+        /* Error 400: auth :(
+        val source = URI("https://adventofcode.com/2023/day/$day/input")
+        val content = source.toURL().openStream().bufferedReader().readLines().joinToString(separator = "\n")
+        val writer = file.bufferedWriter()
+        writer.write(content)
+        writer.flush()
+        writer.close
+        cache[day] = content
+        return content
+        / */
+
+        TODO("Fill file with content: https://adventofcode.com/2023/day/$day/input")
+    }
+}
