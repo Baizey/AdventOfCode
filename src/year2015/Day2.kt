3ed72c0a32473d9d15f1f183593a5ea4708bc71a
Baizey <lassemoerk.mrk@gmail.com>
2023-12-03 00:14:05 +0100

    2015 - d2

--- src/year2015/Day2.kt
+++ src/year2015/Day2.kt
@@ -1,6 +1,38 @@
 package year2015
+
 import utils.*
+import kotlin.math.min
 
 fun main() {
     val lines = Input.get(2015, 2)
+
+    fun part1() {
+        var total = 0L
+        lines.forEach { line ->
+            val sides = line.split("x").map { it.toLong() }
+            val l = sides[0]
+            val w = sides[1]
+            val h = sides[2]
+            val a = l * w
+            val b = w * h
+            val c = h * l
+            total += 2 * (a + b + c) + listOf(a, b, c).min()
+        }
+        println(total)
+    }
+
+    fun part2() {
+        var total = 0L
+        lines.forEach { line ->
+            val sides = line.split("x").map { it.toLong() }
+            val bow = sides.reduce { acc, v -> acc * v }
+            val sorted = sides.sorted()
+            val perimeter = 2 * sorted[0] + 2 * sorted[1]
+            total += bow + perimeter
+        }
+        println(total)
+    }
+
+    part1()
+    part2()
 }
