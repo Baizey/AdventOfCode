d8070d484a16d68f5f124fe78455372cf22ddd58
Baizey <lassemoerk.mrk@gmail.com>
2023-12-03 00:00:10 +0100

    2015 - D1

--- src/year2015/Day1.kt
+++ src/year2015/Day1.kt
@@ -1,6 +1,32 @@
 package year2015
+
 import utils.*
 
 fun main() {
-    val lines = Input.get(2015, 1)
+    val lines = Input.get(2015, 1).joinToString(separator = "")
+
+    fun part1() {
+        var at = 0
+        lines.toCharArray().forEach {
+            if (it == '(') at++
+            else if (it == ')') at--
+        }
+        println(at)
+    }
+
+    fun part2() {
+        var at = 0
+        lines.toCharArray().forEachIndexed { i, it ->
+            if (it == '(') at++
+            else if (it == ')') at--
+            if (at < 0) {
+                println(i + 1)
+                return
+            }
+        }
+    }
+
+    part1()
+    part2()
+
 }
