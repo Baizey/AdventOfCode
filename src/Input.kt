3d85834fb0f9c7e51656ddbb11303dcfff467564
Baizey <lassemoerk.mrk@gmail.com>
2023-12-02 22:35:53 +0100

    how did I write it this poorly ;-;

--- src/Input.kt
+++ src/Input.kt
@@ -1,7 +1,6 @@
 import java.io.File
 import java.io.IOException
 import java.net.URI
-import java.net.URL
 
 object Input {
     private val cache: Array<List<String>> = Array(30) { listOf() }
@@ -9,27 +8,20 @@ object Input {
     fun day(day: Int): List<String> {
         if (cache[day].isNotEmpty()) return cache[day]
 
-        val fileName = "day_$day.txt"
-        val resource: URL? = object {}.javaClass.classLoader.getResource(fileName)
-        if (resource != null) {
-            val lines = resource.openStream().bufferedReader().useLines(Sequence<String>::toList)
-            val content = lines.joinToString(separator = "\n")
+        val file = File("resources\\day_$day.txt")
+        if (file.exists()) {
+            val lines = file.readLines()
             cache[day] = lines
-            if (content.isNotEmpty()) return lines
+            if (lines.isNotEmpty()) return lines
         }
 
-        val fileCache = File("resources\\$fileName")
-        fileCache.parentFile.mkdirs()
-        fileCache.createNewFile()
+        file.parentFile.mkdirs()
+        file.createNewFile()
 
         try {
             val source = URI("https://adventofcode.com/2023/day/$day/input")
             val lines = source.toURL().openStream().bufferedReader().useLines(Sequence<String>::toList)
-            val content = lines.joinToString(separator = "\n")
-            val writer = fileCache.bufferedWriter()
-            writer.write(content)
-            writer.flush()
-            writer.close()
+            file.writeText(lines.joinToString(separator = System.lineSeparator()))
             cache[day] = lines
             return lines
         } catch (e: IOException) {
