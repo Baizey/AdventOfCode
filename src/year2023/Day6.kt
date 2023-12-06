cdba7f2b970f34355ae8d0e40e551144b695d4f6
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2023-12-06 21:50:07 +0100

    2023 d6

--- src/year2023/Day6.kt
+++ src/year2023/Day6.kt
@@ -1,6 +1,40 @@
 package year2023
+
 import utils.*
 
 fun main() {
+    data class Race(val time: Long, val distance: Long)
+
     val lines = Input.get(2023, 6)
+
+    fun part1() {
+        val times = lines.first().split(Regex(" +")).stream().skip(1).map { it.toLong() }.toList()
+        val distances = lines.last().split(Regex(" +")).stream().skip(1).map { it.toLong() }.toList()
+        val races = times.zip(distances) { time, distance -> Race(time, distance) }
+        var result = 1L
+        races.forEach { race ->
+            var chances = 0L
+            for (speed in 1..<race.time) {
+                val distance = speed * (race.time - speed)
+                if (distance > race.distance) chances++
+            }
+            result *= chances
+        }
+        println(result)
+    }
+    part1()
+
+    fun part2(){
+        val time = lines.first().split(":").last().replace(" ", "").toLong()
+        val distance = lines.last().split(":").last().replace(" ", "").toLong()
+        val race = Race(time, distance)
+        var chances = 0L
+        for (speed in 1..<race.time) {
+            val distance = speed * (race.time - speed)
+            if (distance > race.distance) chances++
+        }
+        println(chances)
+    }
+    part2()
+
 }
