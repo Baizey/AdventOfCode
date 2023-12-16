3c7bc1e401ff45950a78d119db171e1cff85b6e0
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2023-12-17 00:16:00 +0100

    2023 d14 part 2

--- src/year2023/Day14.kt
+++ src/year2023/Day14.kt
@@ -6,9 +6,94 @@ fun main() {
     val roundedRock = 'O'
     val cubeRock = '#'
     val emptySpace = '.'
-    val lines = Input.get(2023, 14)
+    val lines = Input.get(2023, 14).map { it.toCharArray() }.toTypedArray()
 
-    fun part1() {
+    fun tiltNorth() {
+        for (x in lines[0].indices) {
+            var currentPos = 0
+            for (y in lines.indices) {
+                when (lines[y][x]) {
+                    roundedRock -> {
+                        lines[y][x] = emptySpace
+                        lines[currentPos][x] = roundedRock
+                        currentPos++
+                    }
+
+                    cubeRock -> {
+                        currentPos = y + 1
+                    }
+                }
+            }
+        }
+    }
+
+    fun tiltSouth() {
+        for (x in lines[0].indices) {
+            var currentPos = lines.lastIndex
+            for (y in lines.lastIndex downTo 0) {
+                when (lines[y][x]) {
+                    roundedRock -> {
+                        lines[y][x] = emptySpace
+                        lines[currentPos][x] = roundedRock
+                        currentPos--
+                    }
+
+                    cubeRock -> {
+                        currentPos = y - 1
+                    }
+                }
+            }
+        }
+    }
+
+    fun tiltWest() {
+        for (y in lines.indices) {
+            var currentPos = 0
+            for (x in lines[0].indices) {
+                when (lines[y][x]) {
+                    roundedRock -> {
+                        lines[y][x] = emptySpace
+                        lines[y][currentPos] = roundedRock
+                        currentPos++
+                    }
+
+                    cubeRock -> {
+                        currentPos = x + 1
+                    }
+
+                }
+            }
+        }
+    }
+
+    fun tiltEast() {
+        for (y in lines.indices) {
+            var currentPos = lines[0].lastIndex
+            for (x in lines[0].lastIndex downTo 0) {
+                when (lines[y][x]) {
+                    roundedRock -> {
+                        lines[y][x] = emptySpace
+                        lines[y][currentPos] = roundedRock
+                        currentPos--
+                    }
+
+                    cubeRock -> {
+                        currentPos = x - 1
+                    }
+
+                }
+            }
+        }
+    }
+
+    fun runCycle() {
+        tiltNorth()
+        tiltWest()
+        tiltSouth()
+        tiltEast()
+    }
+
+    fun calculateNorthLoad(): Long {
         var sum = 0L
         for (x in lines[0].indices) {
             var currentLoad = lines.size
@@ -25,11 +110,59 @@ fun main() {
                 }
             }
         }
-        println(sum)
+        return sum
+    }
+
+    fun calculateLoad(): Long {
+        var sum = 0L
+        for (x in lines[0].indices) {
+            for (y in lines.indices) {
+                when (lines[y][x]) {
+                    roundedRock -> {
+                        sum += lines.size - y
+                    }
+                }
+            }
+        }
+        return sum
+    }
+
+    fun part1() {
+        println(calculateNorthLoad())
     }
 
     fun part2() {
+        val prime = 31L
+        val seen = HashMap<Long, Long>()
+        var i = 0L
+        val cycles = 1_000_000_000L
+        while (i < cycles) {
+            runCycle()
+            //println("After ${i + 1} cycles")
+            //println(lines.joinToString(separator = "\n") { it.joinToString(separator = "") })
+            var hash = 17L
+            for (y in lines.indices) {
+                for (x in lines[0].indices) {
+                    hash = hash * prime + (lines[y][x].code)
+                    hash = hash * prime + x
+                    hash = hash * prime + y
+                }
+            }
 
+            if (seen.contains(hash)) {
+                val cycleSize = i - seen[hash]!!
+                while (i + cycleSize < cycles) i += cycleSize
+                while (++i < cycles) {
+                    runCycle()
+                }
+                println(calculateLoad())
+                return
+            } else {
+                seen[hash] = i
+                i++
+            }
+        }
+        println(calculateNorthLoad())
     }
 
     part1()
