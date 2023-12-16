b9627aab0f209468a28de64fedf0e93cf869c593
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2023-12-17 00:39:32 +0100

    2023 - d15 part 2

--- src/year2023/Day15.kt
+++ src/year2023/Day15.kt
@@ -1,19 +1,18 @@
 package year2023
 
 import utils.*
+import java.util.stream.IntStream
 
 fun main() {
     val words = Input.get(2023, 15).first().split(",")
 
     fun hash(str: String): Int {
         var result = 0
-
         str.forEach {
             result += it.code
             result *= 17
             result %= 256
         }
-
         return result
     }
 
@@ -22,6 +21,26 @@ fun main() {
     }
 
     fun part2() {
+        data class Wrap(val key: String, var value: Int)
+
+        val boxes = IntStream.range(0, 256).mapToObj { mutableListOf<Wrap>() }.toList()
+        words.forEach { word ->
+            val parts = word.split(Regex("[-=]"))
+            val key = parts.first()
+            val box = boxes[hash(key)]
+            if (word.contains("=")) {
+                val value = parts.last().toInt()
+                if (box.any { it.key == key }) {
+                    box.first { it.key == key }.value = value
+                } else box.add(Wrap(key, value))
+            } else if (word.contains("-")) {
+                box.removeIf { it.key == key }
+            }
+        }
+
+        println(boxes.mapIndexed { i, box ->
+            box.mapIndexed { j, wrap -> (i + 1) * (j + 1) * wrap.value }.sum()
+        }.sum())
 
     }
     part1()
