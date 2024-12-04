91343ed104b4e5ceca48813c68161dafab8f3188
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2024-12-04 18:20:32 +0100

    mig

--- src/year2015/Day14.kt
+++ src/year2015/Day14.kt
@@ -2,18 +2,14 @@ package year2015
 import utils.*
 
 fun main() {
-    val lines = Input.get(2015, 14).asLines()
+    val rawInput = Input.get(2015, 14)
     
     fun part1() {
-    
+        
     }
     fun part2() {
-    
-    }
-    fun part3() {
-    
+        
     }
     part1()
     part2()
-    part3()
 }
--- src/year2015/Day20.kt
+++ src/year2015/Day20.kt
@@ -2,18 +2,14 @@ package year2015
 import utils.*
 
 fun main() {
-    val lines = Input.get(2015, 20).asLines()
+    val rawInput = Input.get(2015, 20)
     
     fun part1() {
-    
+        
     }
     fun part2() {
-    
-    }
-    fun part3() {
-    
+        
     }
     part1()
     part2()
-    part3()
 }
