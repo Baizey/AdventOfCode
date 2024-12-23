797b5bb72d99edd9e6a7bda95b086b5d8fe13662
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2024-12-24 00:56:36 +0100

    d23

--- src/year2024/Day23.kt
+++ src/year2024/Day23.kt
@@ -1,14 +1,49 @@
 package year2024
+
 import utils.*
+import utils.Helpers.println
 
 fun main() {
-    val input = Input.get(2024, 23)
-    
+    val edges = Input.get(2024, 23).asWordLines()
+        .flatMap { listOf(it[0] to it[1], it[1] to it[0]) }
+        .groupBy { it.first }.values
+        .associate { it.first().first to it.map { it.second }.toSet() }
+
+    fun String.edges() = edges[this]!!
+
     fun part1() {
-        
+        edges.keys
+            .asSequence()
+            .filter { it.startsWith("t") }
+            .flatMap { first ->
+                val seconds = first.edges()
+                seconds.mapNotNull { second ->
+                    val otherConnects = second.edges()
+                    val thirds = seconds.intersect(otherConnects)
+                    if (thirds.isEmpty()) null else thirds.map { third -> listOf(first, second, third) }
+                }.flatten()
+            }
+            .map { it.sorted().joinToString(separator = ",") }
+            .distinct()
+            .count()
+            .println()
     }
+
     fun part2() {
-        
+        var biggest = setOf<String>()
+        var cliques = edges.keys.flatMap { first -> first.edges().map { second -> setOf(first, second) } }
+            .distinctBy { it.sorted().joinToString(separator = ",") }
+        while (cliques.isNotEmpty()) {
+            biggest = cliques.first().toSet()
+            cliques = cliques
+                .map { clique ->
+                    val newCandidates = clique.first().edges().filter { !clique.contains(it) }
+                    newCandidates.filter { n -> clique.all { n.edges().contains(it) } }.map { clique + setOf(it) }
+                }
+                .flatten()
+                .distinctBy { it.sorted().joinToString(separator = ",") }
+        }
+        biggest.sorted().joinToString(separator = ",").println()
     }
     part1()
     part2()
